42fe4ded3944de5987356a697cdfb0cbda974a0e
Michael Herman <hermanmu@gmail.com>
2014-11-10 11:45:53 -0700

    updates

--- static/main.js
+++ static/main.js
@@ -4,53 +4,68 @@
 
   angular.module('WordcountApp', [])
 
-  .controller('WordcountController', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http, $timeout) {
-    $scope.getResults = function() {
+  .controller('WordcountController', ['$scope', '$log', '$http', '$timeout', 'callAPIService',
+    function($scope, $log, $http, $timeout, callAPIService) {
+      $scope.getResults = function() {
 
-      $log.log("test")
+        $log.log("test")
 
-      // get the URL from the input
-      var userInput = $scope.input_url
+        // get the URL from the input
+        var userInput = $scope.input_url
 
-      // fire the request
-      $http.post('/start', {"url": userInput}).
-        success(function(results) {
-          $log.log(results)
-          getWordCount(results)
+        // fire the request
+        callAPIService.response('post', '/start', {"url": userInput})
+          .success(function(results) {
+            $log.log(results)
+            getWordCount(results)
 
-        }).
-        error(function(error) {
-          $log.log(error)
-        });
+          }).
+          error(function(error) {
+            $log.log(error)
+          });
 
-      // the poller functionality
-      function getWordCount(jobID) {
+        // the poller functionality
+        function getWordCount(jobID) {
+          var timeout = ""
 
-        var timeout = ""
+          var poller = function() {
+            // fire another request
+            callAPIService.response('get', '/results/'+jobID, null)
+              .success(function(data, status, headers, config) {
+                if(status === 202) {
+                  $log.log(data, status)
+                } else if (status === 200){
+                  $log.log(data)
+                  $scope.wordcounts = data;
+                  $timeout.cancel(timeout);
+                  return false;
+                }
+                // continue to call the poller() function every 2 seconds
+                // until the timout is cancelled
+                timeout = $timeout(poller, 2000);
+              });
+          };
+          poller();
+        }
 
-        var poller = function() {
-          // fire another request
-          $http.get('/results/'+jobID).
-            success(function(data, status, headers, config) {
-              if(status === 202) {
-                $log.log(data, status)
-              } else if (status === 200){
-                $log.log(data)
-                $scope.wordcounts = data;
-                $timeout.cancel(timeout);
-                return false;
-              }
-              // continue to call the poller() function every 2 seconds
-              // until the timout is cancelled
-              timeout = $timeout(poller, 2000);
-            });
-        };
-        poller();
       }
-
     }
-  }
 
-  ]);
+  ])
+
+  .factory('callAPIService', ['$http', function($http) {
+
+    var request = function(requestMethod, endpoint, payload) {
+      if (requestMethod === 'post') {
+        return $http.post(endpoint, payload);
+      } else if (requestMethod === 'get') {
+        return $http.get(endpoint)
+      };
+    }
+    return {
+      response: function(requestMethod, endpoint, payload)
+        { return request(requestMethod, endpoint, payload, 'response'); },
+    };
+  }]);
 
 }());
